e2f841478c71dd3b0197aad08a7dc253bc102dc4
abhijeet <redefinedabhi2@gmail.com>
2022-03-18 20:15:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ for i in range(80):
         file.write(d+'\n')
     os.system('git add file.txt')
     os.system('git commit --date=" 2021-'+str(rand)+'-'+d+'" -m 1')
-os.system('git push -u origin main')
+os.system('git push -u origin master')
 
 #git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600" 
 #git fetch origin master
